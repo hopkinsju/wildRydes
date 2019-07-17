8d28400ce222d60443a5fb897c3cf90657794100
Justin Hopkins <hopkinsju@gmail.com>
2019-07-17 14:29:07 +0000

    add cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_0g2g1XWU0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7g7kmbngs8p2m1pdr5vibr9rkm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
